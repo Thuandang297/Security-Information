bb13750454e2f33a7bb563eb0cbe5e79feff00f1
Thuandang297 <Thuandang729@gmail.com>
2021-08-16 00:27:18 +0700

    update the Feister

--- Feistel/Version2.cpp
+++ Feistel/Version2.cpp
@@ -8,26 +8,43 @@ int main()
 	cout<<"Nhap vao plaintext:";cin>>P;
 	 L[0]=P[0];
 	 R[0]=P[1];
-	cout<<"L:"<<L[0]<<endl;
-	cout<<"R:"<<R[0]<<endl;
 	cout<<"Nhap vao chuoi k:";cin>>K;
 
-
+cout<<"K[0]:"<<K[0]<<endl;
+cout<<"L[0]:"<<L[0]<<endl;
+cout<<"R[0]:"<<R[0]<<endl;
     //Ma hoa
-    R[0]=P[0];
-    L[0]=P[1];
-    
     for(int i=1;i<=2;i++)
     {
-    	K[i]=K[0]<<i;
-    	R[i]=L[i-1]^(R[i-1]+K[i]);
-    	L[i]=R[i-1];
+    	K[i]=K[0]<<i;cout<<"K["<<i<<"]:"<<K[i]<<endl;
+    	R[i]=L[i-1]^(R[i-1]+K[i]);cout<<"R["<<i<<"]:"<<R[i]<<endl;
+    	L[i]=R[i-1];cout<<"L["<<i<<"]:"<<L[i]<<endl;
 	}
 	char C[2];
 cout<<"day ma hoa la:"<<endl;
-C[0]=R[2];
-C[1]=L[2];
+C[0]=L[2];
+C[1]=R[2];
 cout<<C<<endl;
+
+
+//Giai ma
+
+ 
+cout<<"K[2]:"<<K[2]<<endl;
+cout<<"L[2]:"<<L[2]<<endl;
+cout<<"R[2]:"<<R[2]<<endl;    
+int j=0;
+    for(int i=1;i>=0;i--)
+    {
+    	j=j+1;
+    	K[i]=K[i];cout<<"K["<<i<<"]:"<<K[i]<<endl;
+    	R[i]=L[i+1];cout<<"R["<<i<<"]:"<<R[i]<<endl;
+    	L[i]=R[i+1]^(R[i]+K[i+1]);cout<<"L["<<i<<"]:"<<L[i]<<endl;
+	}
+cout<<"Chuoi giai ma la:"<<endl;
+P[0]=L[0];
+P[1]=R[0];
+cout<<P<<endl;
 
 
 	return 0;
